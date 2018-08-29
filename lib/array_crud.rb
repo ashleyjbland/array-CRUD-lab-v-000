6a19b221d11fa2cb43fec9854efa74ebefca0ba2
Ashley Bland <ashleyjbland@gmail.com>
2018-08-29 23:22:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,25 +14,29 @@ end
 def add_element_to_start_of_array(array, element)
   array = ["summer", "winter", "fall", "spring"]
   array.unshift("wow")
-
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = ["summer", "winter", "fall", "spring"]
+  spring = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  array = ["summer", "winter", "fall", "spring"]
+  summer = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array = ["summer", "winter", "fall", "spring"]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array = ["summer", "winter", "fall", "spring"]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["summer", "winter", "fall", "spring"]
+  array.last
 end
