28e7f245df0fba201c7506e3095eea73538624f3
Ashley Bland <ashleyjbland@gmail.com>
2018-08-29 23:19:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,6 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["summer", "winter", "fall", "spring"]
+  array.unshift("wow")
 
 end
 
