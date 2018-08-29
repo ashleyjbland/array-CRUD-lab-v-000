94eafe1c7ded2cbd7467ca9d99a23b1a6055b622
Ashley Bland <ashleyjbland@gmail.com>
2018-08-29 23:17:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["summer", "winter", "fall", "spring"]
-  <<"indian summer"
+  array << "indian summer"
 end
 
 def add_element_to_start_of_array(array, element)
